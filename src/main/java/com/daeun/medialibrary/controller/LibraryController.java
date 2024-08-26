9855a8ac5d48a24c93572a444e1ef87653a00f35
최다은 <ed040820@betree.co.kr>
2024-08-26 17:42:19 +0900

    controller 주석추가

--- src/main/java/com/daeun/medialibrary/controller/LibraryController.java
+++ src/main/java/com/daeun/medialibrary/controller/LibraryController.java
@@ -6,11 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,11 @@ public class LibraryController {
         List<LibraryBoardVO> allBoardList = libraryBoardService.getList();
         return ResponseEntity.status(HttpStatus.OK).body(allBoardList);
     }
-    //이미지 상세
+
+    //write(글 등록)
+
+    //이미지 클릭하면 상세로 넘어가기
+    //detail
+
 
 }
